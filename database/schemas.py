eab922697f4ff7fa0a09ac1c9e145cc965a42176
INGANZAMARUMPU <jonkur96@gmail.com>
2021-09-22 13:38:05 +0200

    added all pydantic

--- database/schemas.py
+++ database/schemas.py
@@ -10,6 +10,21 @@ class Roles(Enum):
 	LITTERAIRE = auto()
 	ENSEIGNENT = auto()
 
+class WordTypes(Enum):
+	VERBE = auto()
+	NOM = auto()
+	ADJECTIF = auto()
+	INTERJECTION = auto()
+	ADVERBE = auto()
+	INCONNUE = auto()
+
+class WordStatus(Enum):
+	WAITING = auto()
+	VALIDATED = auto()
+	REJECTED = auto()
+
+# ========== AUTH =========
+
 class Token(BaseModel):
 	access_token: str
 	refresh_token: str
@@ -24,6 +39,8 @@ class RefreshedToken(BaseModel):
 class LoginForm(BaseModel):
 	username: str
 	password: str
+
+# ========== USER =========
 
 class UserBase(BaseModel):
 	email: str
@@ -48,4 +65,50 @@ class User(UserBase):
 	id: int
 	is_active: bool
 
+# ========== INDOBEKO =========
 
+class Indobeko(BaseModel):
+	valeur:str
+	sens_run:Optional[str]
+	sens_fr:Optional[str]
+	sens_en:Optional[str]
+	run_by:Optional[str]
+	fr_by:Optional[str]
+	en_by:Optional[str]
+	run_by:List[str]
+	fr_by:List[str]
+	en_by:List[str]
+	date:Optional[date] = date.today
+
+# ========== INGOBOTOZO =========
+
+class Ingobotozo(BaseModel):
+	valeur:str
+	sens_run:Optional[str]
+	sens_fr:Optional[str]
+	sens_en:Optional[str]
+	date:Optional[date] = date.today
+
+# ========= MOTS =========
+
+class Mot(BaseModel):
+	mot:str
+	categorie:WordTypes = WordTypes.INCONNUE
+	singulars:List[str]
+	plurals:List[str]
+	imvugo:Imvugo
+	base:str
+	synonyme:List[str]
+	sens_run:Optional[str]
+	sens_fr:Optional[str]
+	sens_en:Optional[str]
+	run_by:Optional[str]
+	fr_by:Optional[str]
+	en_by:Optional[str]
+	run_validated_by:Optional[str]
+	fr_validated_by:Optional[str]
+	en_validated_by:Optional[str]
+	word_status:WordStatus = WordStatus.WAITING
+	run_status:WordStatus = WordStatus.WAITING
+	fr_status:WordStatus = WordStatus.WAITING
+	en_status:WordStatus = WordStatus.WAITING
--- views/__init__.py
+++ views/__init__.py
@@ -1,14 +1,4 @@
 from . import (
-	bien,
-	categorie,
-	contribuable,
-	details_recouvrement,
-	mode_payment,
-	achats,
-	percepteur,
-	perception,
-	recouvrement,
-	outils,
 	user,
 	authentication
 )
